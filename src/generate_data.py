c7a83e1b32c046902f5454c8a0b3727c734ce68a
Siddarth Shantinath Patil <siddarth.patil@creditsafe.com>
2022-12-26 00:55:22 +0000

    added complete list of available cities

--- src/generate_data.py
+++ src/generate_data.py
@@ -3,6 +3,8 @@ import time
 
 from dotenv import load_dotenv
 
+from available_cities import available_cities
+
 load_dotenv(".env")
 
 
@@ -33,7 +35,7 @@ class GenerateLink:
             list(range(3000, 20000, 500))
         )
         self.available_beds = list(range(1, 16))
-        self.available_bath = 
+        self.available_bath = list(range(1, 6))
 
     def generate_filter_link(self):
         max_price = os.environ.get("max_price")
@@ -41,18 +43,33 @@ class GenerateLink:
         max_bed = os.environ.get("max_bed")
         min_bath = os.environ.get("min_bath")
 
-        link = f"https://www.daft.ie/property-for-rent/ireland?rentalPrice_to={max_price}&numBeds_from={min_bed}&numBeds_to={max_bed}&numBaths_from={min_bath}&sort=publishDateDesc&location={city_name}&location={city_name}-city"
+        self.city_filter_link()
+
+        print("Final link is: ", self.link)
         return self.link
 
     def city_filter_link(self):
         try:
             city_name = os.environ.get("city_name")
-            if not (isinstance(city_name, str) and max_hours > 0 and max_hours <= 9):
+            if not (
+                isinstance(city_name, list)
+                and all(isinstance(item, str) for item in city_name)
+            ):
                 print(
-                    "Conditions for Maximum hours not satisfied, therefore, running the bot for 2 hours"
+                    "City Name should be a list and all items inside it should be string"
+                    # TODO: Raise Custom Expection and stop execution
                 )
-                max_hours = 2
+            elif not all(item in available_cities for item in city_name):
+                print("City name/s should be from the list provided")
+                # TODO: Raise Custom Expection and stop execution
+            elif len(city_name) == 1:
+                self.link = f"https://www.daft.ie/property-for-rent/{city_name[0]}"
+                return self.link
+            else:
+                self.link += "?"
+                for item in city_name:
+                    self.link += f"location={item}&"
+                return self.link[:-1]
         except NameError:
-            # Running it for 2 hours by default
-            print("Running the bot for 2 hours as there were no hours specified.")
-            max_hours = 2
+            # TODO: Raise custom Expection
+            pass
